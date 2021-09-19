1c0c0a611591d29610719791193c74b89b38a4ef
Alex Gavin <gavina2@wwu.edu>
2021-09-19 11:45:50 -0700

    Implement VM enums, more LC3 init
    VM enums implemented:
    - General purpose registers (GpRegister)
    - Condition flags (Flag)
    - Instructions (Instruction)
    
    LC3 init: initializing default values for registers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
+/* ~~~ Constants and enums ~~~ */
+const UINT16_MAX: usize = 65536;
+const PRGM_START_ADDR: usize = 0x3000;
+
+#[derive(Debug, Enum)]
+enum GpRegister {
+    R0, R1, R2, R3, R4, R5, R6, R7
+}
+
+#[derive(Debug, Enum)]
+enum Flag {
+    POS,
+    ZRO,
+    NEG
+}
+
+#[derive(Debug)]
+enum Instruction {
+    BR,     // Branch
+    ADD,    // Add
+    LD,     // Load
+    ST,     // Store
+    JSR,    // Jump register
+    AND,    // Bitwise AND
+    LDR,    // Load register
+    STR,    // Store register
+    RTI,    // Unused
+    NOT,    // Bitwise NOT
+    LDI,    // Load indirect
+    STI,    // Store indirect
+    JMP,    // Jump
+    RES,    // Reserved (unused)
+    LEA,    // Load effective address
+    TRAP    // Execute trap
+}
+
+
+/* ~~~ Imports ~~~ */
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 use std::process::exit;
+
 use structopt::StructOpt;
+use enum_map::{enum_map, Enum, EnumMap};
 
-// Constants
-const UINT16_MAX: usize = 65536;
-const PRGM_START_ADDR: usize = 12288;
+use Flag::*;
+use GpRegister::*;
 
 
+/* ~~~ Structs ~~~ */
 // CLI Parsing
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
@@ -20,12 +60,27 @@ struct Cli {
 // LC-3 VM
 pub struct LC3 {
     memory: Option<[u8; UINT16_MAX]>,
+    gp_regs: EnumMap<GpRegister, u16>,
+    pc: usize,
+    cond: Flag
 }
 
 impl Default for LC3 {
     fn default() -> LC3 {
         LC3 {
             memory: None,
+            gp_regs: enum_map! {
+                R0 => 0,
+                R1 => 0,
+                R2 => 0,
+                R3 => 0,
+                R4 => 0,
+                R5 => 0,
+                R6 => 0,
+                R7 => 0,
+            },
+            pc: PRGM_START_ADDR,
+            cond: ZRO
         }
     }
 }
@@ -53,16 +108,18 @@ impl LC3 {
         assert!(n < (UINT16_MAX - PRGM_START_ADDR), "Cannot run program of size larger than {}", UINT16_MAX - PRGM_START_ADDR);
 
 
-        LC3 {
-            memory: Some(memory)
-        }
+        LC3 { memory: Some(memory), ..Default::default() }
     }
 }
 
 
+/* ~~~ The fun stuff ~~~ */
 fn main() {
     let args = Cli::from_args();
     let lc3 = LC3::new(args.program_file);
 
     println!("memory: {:?}", lc3.memory.unwrap());
+    println!("gp_regs: {:?}", lc3.gp_regs);
+    println!("PC: {:#04x}", lc3.pc);
+    println!("COND: {:?}", lc3.cond);
 }
