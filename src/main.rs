0c47951e69024d06f574c43eea85676d65727876
Alex Gavin <gavina2@wwu.edu>
2021-09-19 11:10:20 -0700

    Impl prgrm file read into memory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::fs::File;
+use std::io::Read;
 use std::path::PathBuf;
 use std::process::exit;
 use structopt::StructOpt;
+
+// Constants
+const UINT16_MAX: usize = 65536;
+const PRGM_START_ADDR: usize = 12288;
 
 
 // CLI Parsing
@@ -14,13 +19,21 @@ struct Cli {
 
 // LC-3 VM
 pub struct LC3 {
-    program_file: File
+    memory: Option<[u8; UINT16_MAX]>,
+}
+
+impl Default for LC3 {
+    fn default() -> LC3 {
+        LC3 {
+            memory: None,
+        }
+    }
 }
 
 impl LC3 {
     pub fn new(program_file: PathBuf) -> LC3 {
         // Open VM image file
-        let program_file = match File::open(&program_file) {
+        let mut program_file = match File::open(&program_file) {
             Ok(file) => file,
             Err(error) => {
                 eprintln!("error: couldn't open {:?}, {}", program_file, error);
@@ -28,8 +41,20 @@ impl LC3 {
             }
         };
 
+        // Read program into memory
+        let mut memory: [u8; UINT16_MAX] = [0; UINT16_MAX];
+        let n = match program_file.read(&mut memory[PRGM_START_ADDR..]) {
+            Ok(n) => n,
+            Err(error) => {
+                eprintln!("error: couldn't read {:?}, {}", program_file, error);
+                exit(-1)
+            }
+        };
+        assert!(n < (UINT16_MAX - PRGM_START_ADDR), "Cannot run program of size larger than {}", UINT16_MAX - PRGM_START_ADDR);
+
+
         LC3 {
-            program_file: program_file
+            memory: Some(memory)
         }
     }
 }
@@ -39,5 +64,5 @@ fn main() {
     let args = Cli::from_args();
     let lc3 = LC3::new(args.program_file);
 
-    println!("{:?}", lc3.program_file);
+    println!("memory: {:?}", lc3.memory.unwrap());
 }
